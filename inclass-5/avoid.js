a8e82b547726d855b90d866a90303b4a62a28907
jieya907 <wenjieyas@gmail.com>
2017-01-24 18:44:36 -0600

    added more functions

--- inclass-5/avoid.js
+++ inclass-5/avoid.js
@@ -1,14 +1,45 @@
 window.onload = function (){
+    let shift_pressed = false;
     let btn = document.getElementById("click_btn");
 
-    let movebtn = function (){
+    const movebtn = function (){
         console.log("mouse approaching");
-        let direction = ["left","right","top", "bottom"];
-        const dir = Math.floor((Math.random() * 4));
-        const dist = Math.floor((Math.random() * 50) + 8);
-        const move = "padding-"+ direction[dir]+":"+dist+"em;";
-        document.getElementById("btn_box").style = move;
-        console.log(move);
-    }
+        if (!shift_pressed) {
+            console.log("moving");
+            let direction = ["left","top"];
+            const dir = Math.floor((Math.random() * 2));
+            const dist = Math.floor((Math.random() * 50) + 8);
+            const move = "padding-"+ direction[dir]+":"+dist+"em;";
+            document.getElementById("btn_box").style = move;
+        }
+    };
+
+    const showcong = function () {
+        let cong = document.createElement('div');
+        cong.innerHTML = "Congratulations! <br> You won!";
+        cong.style.backgroundColor = "green";
+        cong.style.bordeColor = "red";
+        cong.style.borderWidth = "2em";
+        document.getElementsByTagName("body")[0].appendChild(cong);
+    };
+
+    const stop = function (e) {
+        if (e.which || e.keyCode === 65) {
+            shift_pressed = true;
+        }
+    };
+
+    const back = function (e) {
+        if (e.which || e.keyCode === 65) {
+            shift_pressed = false;
+        }
+    };
+
     btn.addEventListener("mouseover", movebtn);
+    document.addEventListener("keydown", stop);
+    document.addEventListener("keyup", back);
+
+    btn.addEventListener("click", showcong);
 }
+
+
