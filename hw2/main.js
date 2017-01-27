fadaa6410e9d8588960b9e3a8a999706d69564cc
jieya907 <wenjieyas@gmail.com>
2017-01-27 15:48:42 -0600

    final commit

--- hw2/main.js
+++ hw2/main.js
@@ -8,18 +8,28 @@ window.onload = function () {
         {"id": "img4", "backup":["http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-12.jpg","http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-13.jpg","http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-14.jpg", "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg"]},
     ]
 
-
+    // Setting the image rotation, stop and restart interval
+    // button for each image
     img_rotate.forEach(function(item) {
         let idx = 0;
         const imageUpdateRate = (0.5*Math.random()+1) * 500; 
+        
+        //cache the images for rotating
+        let imgList = item.backup.map(function (src) {
+            let img = new Image();
+            img.src = src;
+            img.id = item.id;
+            return img;
+        });
 
         let img_obj = document.getElementById(item.id);
         const rotate = function() {
             idx = (idx+1) % item.backup.length;
-            document.getElementById(item.id).src = item.backup[idx];
+            document.getElementById(item.id).src = imgList[idx].src; 
         };
 
         let interval = setInterval(rotate, imageUpdateRate);
+        // so that we can access the bottons in the same cell of the table
         const cell = img_obj.parentNode.parentNode;
 
         const stop_btn = cell.getElementsByClassName("stop")[0];
--- hw2/profile.js
+++ hw2/profile.js
@@ -1,14 +1,19 @@
 window.onload = function() {
     document.getElementById("my_button").addEventListener("click", clicked);
 
+    // so we can show appropriate help text at the beginning of the form
     const help_text = {"name":"Your full name", "email":"Your email address",
-        "phone": "Your phone number. Digits only", "birthday":"Your date of birth, must be over 16",
-        "zipcode":"Zipcode, digits only", "password":"Your password, more than 6 digits","passconfirm":"Confirm your password, must be the same"}
+        "phone": "Your phone number. Digits only", 
+        "birthday":"Your date of birth, must be over 16",
+        "zipcode":"Zipcode, digits only", 
+        "password":"Your password, more than 6 digits",
+        "passconfirm":"Confirm your password, must be the same"}
 
     function clicked() {
         document.getElementsByTagName("div")
 
         const fields = document.getElementsByClassName("field");
+        // fields is not an array, bind forEach function to it.
         [].forEach.call(fields,function (item) {
             const input =item.getElementsByTagName("input")[0];
             const entered = input.value;
