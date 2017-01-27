325615ea68139b2782f9910a6dbaec9fda9179a0
jieya907 <wenjieyas@gmail.com>
2017-01-26 22:08:06 -0600

    done

--- inclass-6/skyline_files/skyline.js
+++ inclass-6/skyline_files/skyline.js
@@ -24,8 +24,24 @@ var createApp = function(canvas) {
         this.h = h;
         this.clr = clr;
     }
-    
+
     let all_blgs = [];
+
+    canvas.addEventListener("click", function (e) {
+        console.log(e.offsetX + " " + e.offsetY);
+        all_blgs.forEach(function (item) {
+            const x = e.offsetX;
+            const y = e.offsetY;
+
+            const left = item.x;
+            const right = item.x + item.y;
+            const btop = floor - item.h;
+            console.log(right + " " + left + " " + floor + " " + btop); 
+            if (right > x && left <= x && floor > y && btop <= y) {
+                item.h += 5;
+            }
+        });
+    });
 
     var add_building = function () {
         var x0 = Math.random()*canvas.width
@@ -66,17 +82,26 @@ var createApp = function(canvas) {
 
     let x = 0;
 
+    function drawCar(x){
+        c.fillStyle = "blue";
+        c.fillRect(x, floor - 15, 20, 10);
+        fillCircle(x+17, floor -3, 3, "red");
+
+        fillCircle(x+2, floor -3, 3, "red");
+    }
+
     function movingCircle () {
-        c.clearRect(0,0,c.canvas.width,100); 
-        
-        fillCircle (x % canvas.width, 60, 30, "yellow");
+        c.clearRect(0,0,c.canvas.width,c.canvas.height - floor); 
+
+        fillCircle (x, 60, 30, "yellow");
         all_blgs.forEach(function(blg) {
             build(blg.x, blg.y, blg.h, blg.clr);
         });
-        x += 5;
+        drawCar(x); 
+        x = (x + 5) % canvas.width;
         setTimeout(movingCircle, 40);
     }
-    // TODO: make a building grow and add a car.
+    // TODO: add a car.
     movingCircle();
     return {
         build: add_building
